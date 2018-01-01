ae905441fd29d61469270056fc926ad43c40e4fc
Clastres Térence <t.clastres@gmail.com>
2018-01-01 21:25:10 +0100

    Use fontawesome-mixed by default

--- build.sh
+++ build.sh
@@ -7,22 +7,23 @@ mkdir -p ./.work
 
 echo 'build all symbols fonts...'
 cp ./fonts/pomicons-regular.ttf ./build
-cp ./fonts/fontawesome-regular.ttf ./build
+cp ./fonts/fontawesome-mixed.ttf ./build
 cp ./fonts/devicons-regular.ttf ./build
 
-./scripts/fu-relocate ./fonts/octicons-regular.ttf --save-as='.work/octicons-regular-relocated.ttf' --to='0xf300' 2> /dev/null
+./scripts/fu-relocate ./fonts/octicons-regular.ttf --save-as='.work/octicons-regular-relocated.ttf' --from="0xf43e" --to='0xf7a0' 2> /dev/null
 cp ./.work/octicons-regular-relocated.ttf ./build/octicons-regular.ttf
 
 echo 'export maps for all fonts...'
 ./scripts/fu-map ./build/pomicons-regular.ttf --namespace 'POMICONS' 2> /dev/null > ./build/pomicons-regular.sh
 ./scripts/fu-map ./build/octicons-regular.ttf --namespace 'OCTICONS' 2> /dev/null > ./build/octicons-regular.sh
-./scripts/fu-map ./build/fontawesome-regular.ttf --namespace 'AWESOME' 2> /dev/null > ./build/fontawesome-regular.sh
+./scripts/fu-map ./build/fontawesome-mixed.ttf --namespace 'AWESOME' 2> /dev/null > ./build/fontawesome-mixed.sh
 ./scripts/fu-map ./build/devicons-regular.ttf --namespace 'DEVICONS' 2> /dev/null > ./build/devicons-regular.sh
 
 echo 'override supplied font maps...'
 for file in ./fonts/*.sh; do
   [ -f "$file" ] && cp $file ./build
 done
+
 
 echo 'check if fonts are overlapping...'
 cat ./build/*.sh | \
